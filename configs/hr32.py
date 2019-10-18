9c18da13762e48e782fa21af53256e94206764a8
tascj <tascj0@gmail.com>
2019-10-18 13:20:11 +0800

    to exactly match best submission

--- configs/hr32.py
+++ configs/hr32.py
@@ -224,7 +224,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleAug',
-        img_scale=[size / 1024 for size in range(640, 1280 + 1, 128)],
+        img_scale=[size / 1024 for size in range(768, 1280 + 1, 128)],
         transforms=[
             dict(type='Resize', keep_ratio=True),
             # dict(type='RandomFlip'),
